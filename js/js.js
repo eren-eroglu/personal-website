18da53560af3c9a9949303c23e9b52915c5d8b98
Eren <vrsozluk@gmail.com>
2022-10-20 00:18:23 +0300

    social media link has been added

--- js/js.js
+++ js/js.js
@@ -8,7 +8,13 @@ contact.addEventListener("click", myFunction);
 function myFunction(){
     document.querySelector('.main-text').textContent = 'contact'
     document.querySelector('.sub-text').textContent = 'check out my social media'
-    
+    const aiki = document.createElement("a");
+    document.querySelector('.last-text').textContent = 
+    aiki.setAttribute("class", "aiki");
+    aiki.setAttribute("class", "bx bxl-twitter");
+    aiki.setAttribute("href", "https://mobile.twitter.com/35ereneroglu");
+    document.querySelector('.last-text').appendChild(aiki)
+  
     
     
    
