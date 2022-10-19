c109436deba1a7917e98aa67f6b43efcac67fb19
Eren <vrsozluk@gmail.com>
2022-10-20 00:39:28 +0300

    social media accounts has been added

--- js/js.js
+++ js/js.js
@@ -8,12 +8,18 @@ contact.addEventListener("click", myFunction);
 function myFunction(){
     document.querySelector('.main-text').textContent = 'contact'
     document.querySelector('.sub-text').textContent = 'check out my social media'
+    
     const aiki = document.createElement("a");
+    const auc = document.createElement("a");
     document.querySelector('.last-text').textContent = 
-    aiki.setAttribute("class", "aiki");
-    aiki.setAttribute("class", "bx bxl-twitter");
+    aiki.setAttribute("class", "aiki bx bxl-twitter");
     aiki.setAttribute("href", "https://mobile.twitter.com/35ereneroglu");
-    document.querySelector('.last-text').appendChild(aiki)
+    
+    auc.setAttribute("class","auc bx bxl-github");
+    auc.setAttribute("href", "https://github.com/eren-eroglu");
+    document.querySelector('.last-text').appendChild(aiki);
+    document.querySelector('.last-text').appendChild(auc);  
+    
   
     
     
