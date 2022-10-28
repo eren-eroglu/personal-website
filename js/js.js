2ecb8334e35e9fd648ea3cc01308d838fce352e4
Eren <vrsozluk@gmail.com>
2022-10-28 16:54:56 +0300

    Animation has been added to the circles

--- js/js.js
+++ js/js.js
@@ -1,7 +1,8 @@
-const mouseCircle = document.querySelector('.mouse-circle');
-const mouseDot = document.querySelector(".mouse-dot");
+
 
 // Mouse Circle
+const mouseCircle = document.querySelector('.mouse-circle');
+const mouseDot = document.querySelector(".mouse-dot");
 
 const mouseCircleFn = (x,y) => {
     mouseCircle.style.cssText = `top:${y}px; left:${x}px; opacity:1`;
@@ -13,6 +14,8 @@ document.body.addEventListener('mousemove',(e) => {
     let y = e.clientY;
     document.title = "Eren Eroglu - Developer"
     mouseCircleFn(x,y)
+
+    animateCircles(e,x,y)
 })
 
 document.body.addEventListener('mouseleave',() => {
@@ -22,3 +25,44 @@ document.body.addEventListener('mouseleave',() => {
 })
 // End of Mouse Circle
 
+// Animated Circles
+
+const circles = document.querySelectorAll('.circle');
+const mainImg = document.querySelector('.main-circle img');
+
+let mX = 0;
+let mY = 0;
+const z = 50;
+
+const animateCircles = (e,x,y) => {
+
+    if(x < mX){
+        circles.forEach(circle => {
+            circle.style.left = `${z}px`;
+        });
+        mainImg.style.left = `${z}px`;
+    }else if( x > mX){
+        circles.forEach(circle => {
+            circle.style.left = `-${z}px`;
+        });
+        mainImg.style.left = `-${z}px`;
+    }
+
+    if(y<mY){
+        circles.forEach(circle => {
+            circle.style.top = `${z}px`;
+        });
+        mainImg.style.top = `${z}px`;
+    }else if(y>mY){
+        circles.forEach(circle => {
+            circle.style.top = `-${z}px`;
+        });
+        mainImg.style.top = `-${z}px`;
+    }
+
+    mX = e.clientX;
+    mY = e.clientY;
+
+}
+
+//End of Animated Circles
