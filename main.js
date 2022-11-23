a7aaedb05fb707f449c619f9a4be3056f531c5ef
Eren <vrsozluk@gmail.com>
2022-11-23 18:39:12 +0300

    preloader has been fixed

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-
-document.addEventListener("DOMContentLoaded", function(){
-  window.addEventListener('scroll', function() {
-      if (window.scrollY > 50) {
-        document.getElementById('navbar_top').classList.add('fixed-top');
-        // add padding top to show content behind navbar
-        navbar_height = document.querySelector('.navbar').offsetHeight;
-        document.body.style.paddingTop = navbar_height + 'px';
-      } else {
-        document.getElementById('navbar_top').classList.remove('fixed-top');
-         // remove padding top from body
-        document.body.style.paddingTop = '0';
-      } 
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      document.getElementById("navbar_top").classList.add("fixed-top");
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector(".navbar").offsetHeight;
+      document.body.style.paddingTop = navbar_height + "px";
+    } else {
+      document.getElementById("navbar_top").classList.remove("fixed-top");
+      // remove padding top from body
+      document.body.style.paddingTop = "0";
+    }
   });
-}); 
-
+});
 
 const images = document.querySelectorAll(".pr-1");
 
@@ -23,7 +21,6 @@ images.forEach((image) => {
       document.querySelector("#header").style.filter = `blur(5px)`;
       document.querySelector("#hero").style.filter = `blur(5px)`;
       document.querySelector("#hero2").style.filter = `blur(5px)`;
-      
     };
   }
 });
@@ -38,14 +35,15 @@ images.forEach((image) => {
   }
 });
 
+const preloader_container = document.querySelector(".preloader-container");
 
-const preloader_container = document.querySelector('.preloader-container');
+const preloader = document.querySelector(".preloader");
 
-const preloader = document.querySelector('.preloader')
+window.onload = hide();
 
-window.onload = hide()
-
-function hide(){
-  preloader_container.classList.add('hide')
-  preloader.classList.add('hide')
+function hide() {
+  setTimeout(() => {
+    preloader_container.classList.add("hide");
+    preloader.classList.add("hide");
+  }, 1500);
 }
